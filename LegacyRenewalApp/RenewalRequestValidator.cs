fd1201b651e778b3bee4ffdbb6d3b21826dd95ec
MRDrewiena1 <kontad.dr@gmail.com>
2026-04-14 21:31:18 +0200

    +add class with 2 validation methods for data in SubscriptionRenevalService

---
+++ LegacyRenewalApp/RenewalRequestValidator.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace LegacyRenewalApp;
+
+public class RenewalRequestValidator
+{
+    public void ValidateInput(int customerId, string planCode, int seatCount, string paymentMethod)
+    {
+        if (customerId <= 0)
+        {
+            throw new ArgumentException("Customer id must be positive");
+        }
+
+        if (string.IsNullOrWhiteSpace(planCode))
+        {
+            throw new ArgumentException("Plan code is required");
+        }
+
+        if (seatCount <= 0)
+        {
+            throw new ArgumentException("Seat count must be positive");
+        }
+
+        if (string.IsNullOrWhiteSpace(paymentMethod))
+        {
+            throw new ArgumentException("Payment method is required");
+        }
+    }
+
+    public void ValidateCustomerIsActive(Customer customer)
+    {
+        if (!customer.IsActive)
+        {
+            throw new InvalidOperationException("Inactive customers cannot renew subscriptions");
+        }
+    }
+}
